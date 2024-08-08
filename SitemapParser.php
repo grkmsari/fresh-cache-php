0c582fd3b04ce025087d6569238677d00db1fd18
Görkem SARI <gorkem.sari@gmail.com>
2024-08-08 23:02:59 +0300

    Add sitemap URL counts to the output

--- SitemapParser.php
+++ SitemapParser.php
@@ -2,6 +2,7 @@
 
 class SitemapParser {
     private $urls = [];
+    private $sitemaps = [];
 
     // Fetch the sitemap and extract URLs
     public function fetchSitemap($sitemapUrl) {
@@ -17,14 +18,19 @@ class SitemapParser {
             return;
         }
 
-        $this->parseSitemap($xml);
+        $this->parseSitemap($xml, $sitemapUrl);
     }
 
     // Parse the sitemap XML and extract URLs
-    private function parseSitemap($xml) {
+    private function parseSitemap($xml, $sitemapUrl) {
+        $urlCount = 0;
+
         foreach ($xml->url as $urlElement) {
             $this->urls[] = (string) $urlElement->loc;
+            $urlCount++;
         }
+
+        $this->sitemaps[$sitemapUrl] = $urlCount;
 
         foreach ($xml->sitemap as $sitemapElement) {
             $this->fetchSitemap((string) $sitemapElement->loc);
@@ -34,5 +40,10 @@ class SitemapParser {
     // Get the list of URLs
     public function getUrls() {
         return $this->urls;
+    }
+
+    // Get the list of sitemaps and their URL counts
+    public function getSitemaps() {
+        return $this->sitemaps;
     }
 }
--- cache_refresh.php
+++ cache_refresh.php
@@ -8,6 +8,7 @@ $parser = new SitemapParser();
 $parser->fetchSitemap($sitemapUrl);
 
 $urls = $parser->getUrls();
+$sitemaps = $parser->getSitemaps();
 
 $totalUrls = count($urls);
 
@@ -17,6 +18,10 @@ if ($totalUrls > 0) {
 //    foreach ($urls as $url) {
 //        echo $url . "\n";
 //    }
+    echo "\nSitemaps and their URL counts:\n";
+    foreach ($sitemaps as $sitemap => $count) {
+        echo "$sitemap: $count URLs\n";
+    }
 } else {
     echo "No URLs found in the sitemap.\n";
 }
